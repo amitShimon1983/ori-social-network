7534eb69d84faee4fcc0087cd720f05b0f6d3580
amitvolga <amit.shimon@gmail.com>
2022-07-02 19:46:51 +0300

    ADD: adding init db

--- server/src/app/configureApp.ts
+++ server/src/app/configureApp.ts
@@ -3,9 +3,11 @@ import express, { Express } from 'express';
 import cookieParser from "cookie-parser";
 import { corsMiddleware } from "./middleware";
 import configureRoutes from "./route";
+import { initDb } from "./clients";
 export const configureApp = async (app: Express, appConfig: Configuration) => {
     app.use(cookieParser());
     app.use(express.json({ limit: '50mb' }));
     app.use(corsMiddleware(app, { allowedOrigins: appConfig.allowedOrigins }));
-    configureRoutes(app)
+    configureRoutes(app);
+    await initDb(appConfig.dbConnectionString || '');
 }
