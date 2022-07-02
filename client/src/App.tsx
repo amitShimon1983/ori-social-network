1163d24c299dc3b0c0228d5d87d96d9b5720adcf
amitvolga <amit.shimon@gmail.com>
2022-07-02 20:14:20 +0300

    Fix: refactor app to support user authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Authenticate, LoginForm, SignUpForm } from './components';
+import { Authenticate, Home, LoginForm, ProtectedRoute, SignUpForm } from './components';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Button } from './components';
 function App() {
+
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/")
   }
+
   return (
     <div className="App">
-      <h1 onClick={handleClick}>Ori Social network</h1>
+      <Button handleClick={handleClick}>Go big or go home</Button>
+      <h1 >Ori Social network</h1>
       <Routes>
-        <Route path="/login" element={<LoginForm />} />
         <Route path="/" element={<Authenticate />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route path="sign-up" element={<SignUpForm />} />
+        <Route
+          path="home"
+          element={
+            <ProtectedRoute user={undefined}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
