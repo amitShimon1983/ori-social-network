3fbf43bbf7ad064f14ddfe2e1024d0c993a05a16
amitvolga <amit.shimon@gmail.com>
2022-07-02 20:58:12 +0300

    Fix: fixing logic for register

--- client/src/components/authenticate/signup/SignUpForm.tsx
+++ client/src/components/authenticate/signup/SignUpForm.tsx
@@ -1,28 +1,43 @@
 import { FunctionComponent, useState } from "react";
+import { isTemplateTail } from "typescript";
+import { validateEmail } from "../../../utils";
 import { Input } from "../../shared";
 import Button from "../../shared/button/Button";
 
 interface SignUpFormProps {
 
 }
-interface signUp { email: string; password: string; confirmPassword: string; }
-const initialState = { email: '', password: '', confirmPassword: '' }
+interface signUp { name: string, email: string; password: string; confirmPassword: string; avatar: string; }
+const initialState = { email: '', password: '', confirmPassword: '', name: '', avatar: '' }
 const SignUpForm: FunctionComponent<SignUpFormProps> = () => {
+
     const [signUp, setSignUp] = useState<signUp>(initialState)
     const [isValid, setIsValid] = useState<boolean>(false)
+
     const handleChange = ({ target }: { target: any }) => {
-        console.log(target?.name);
-        console.log(target?.value);
-        setSignUp((prev) => ({ ...prev, [target?.name]: target?.value }))
+        setSignUp((prev) => {
+            const newState = { ...prev, [target?.name]: target?.value };
+            if ((newState.confirmPassword && newState.password && newState.confirmPassword === newState.password)
+                && (newState.email && validateEmail(newState.email))
+                && newState.name) {
+                setIsValid(true);
+            } else {
+                setIsValid(false);
+            }
+            return (newState)
+        })
     }
+
     const handleSubmit = (event: any) => {
         event.preventDefault();
-        //logic
+        //send to server
         setSignUp(initialState)
     }
+
     return (<>
         <form>
             <Input handleChange={handleChange} placeholder='Email' type='email' name={'email'} value={signUp?.email} required />
+            <Input handleChange={handleChange} placeholder='Name' type='text' name={'name'} value={signUp?.name} required />
             <Input handleChange={handleChange} placeholder='password' type='password' name='password' value={signUp?.password} required />
             <Input handleChange={handleChange} placeholder='Confirm password' type='password' name='confirmPassword' value={signUp?.confirmPassword} required />
             <Button disabled={!isValid} handleClick={handleSubmit}>Submit</Button>
