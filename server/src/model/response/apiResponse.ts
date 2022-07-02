98160ec9f8f226cb7dbd625a7a55379211c0b21e
amitvolga <amit.shimon@gmail.com>
2022-07-02 22:27:33 +0300

    Fix: adding status code to response

--- server/src/model/response/apiResponse.ts
+++ server/src/model/response/apiResponse.ts
@@ -1,10 +1,13 @@
 export class ApiResponse {
     payload: any;
     errors?: string[];
-    constructor(payload: any) {
-        this.payload = payload;
-    }
+    status: number = 400;
     setErrors(errors: string[]) {
         this.errors = errors;
+        this.status = 400;
+    }
+    setPayload(payload: any) {
+        this.payload = payload;
+        this.status = 200;
     }
 }
