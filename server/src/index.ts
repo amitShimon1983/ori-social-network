9741eda1833752ce560adb83ded924cd9a4122e8
amitvolga <amit.shimon@gmail.com>
2022-07-02 19:29:19 +0300

    FIx: refactoring app main function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,24 @@
 
-const express = require('express')
-const app = express()
-const port = 3978
+import express from 'express';
+import mongoose from 'mongoose';
+import { configureApp } from './app/configureApp';
+import { appConfig } from './config';
 
-app.get('/', (req: any, res: any) => {
-    res.send('Hello World!')
-})
+const app = express();
+const port = appConfig.port;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+async function main() {
+     configureApp(app, appConfig);
+    if (appConfig?.dbConnectionString) {
+        mongoose.connect(appConfig.dbConnectionString);
+    }
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    });
+}
+
+
+main().then(() => {
+
+
+});
