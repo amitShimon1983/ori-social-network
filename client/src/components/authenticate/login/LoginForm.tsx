5cd41f05ba2a186a1cd146e715b523395d601351
amitvolga <amit.shimon@gmail.com>
2022-07-09 23:07:07 +0300

    Fix: adding error handling, set user to appVar

--- client/src/components/authenticate/login/LoginForm.tsx
+++ client/src/components/authenticate/login/LoginForm.tsx
@@ -1,23 +1,23 @@
 import { FunctionComponent, useState } from "react";
 import { useNavigate } from "react-router";
 import { authService } from "../../../services";
+import { appContextVar } from "../../../services/store";
 import { validateEmail } from "../../../utils";
 import { Input, Button } from "../../shared";
 
 
 interface LoginFormProps {
-    setUser: React.Dispatch<React.SetStateAction<{
-        [key: string]: any;
-    } | undefined>>
+
 }
 const initialState = { email: '', password: '' };
 interface Login {
     email: string;
     password: string;
 }
-const LoginForm: FunctionComponent<LoginFormProps> = ({ setUser }) => {
+const LoginForm: FunctionComponent<LoginFormProps> = () => {
     const [login, setLogin] = useState<Login>(initialState);
     const [isValid, setIsValid] = useState<boolean>(false)
+    const [errors, setErrors] = useState<string[]>([])
     const navigate = useNavigate();
 
     const handleChange = ({ target }: { target: any }) => {
@@ -31,21 +31,25 @@ const LoginForm: FunctionComponent<LoginFormProps> = ({ setUser }) => {
     }
 
     const onSuccess = (payload: any) => {
-        setUser({ user: payload, isAuthenticate: true })
+        appContextVar({ user: payload, isAuthenticate: true })
         navigate("/home");
+    }
+    const onError = (payload: string[]) => {
+        setErrors(payload)
     }
 
     const handleSubmit = async (event: any) => {
         event.preventDefault();
         await authService.login(login, (payload) => {
             onSuccess(payload)
-        })
+        }, onError)
     }
 
     return (<form>
         <Input value={login.email} placeholder='Email' type='email' name='email' required handleChange={handleChange} />
         <Input value={login.password} placeholder="Password" type='password' name='password' required handleChange={handleChange} />
         <Button disabled={!isValid} handleClick={handleSubmit}>Sign in...</Button>
+        {!!errors.length && errors.map(error => (<div>{error}</div>))}
     </form>);
 }
 
