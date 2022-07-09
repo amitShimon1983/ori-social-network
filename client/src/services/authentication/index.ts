a1612b7c8aa44eafa076f0778b5f22073f2c71a2
amitvolga <amit.shimon@gmail.com>
2022-07-09 23:09:38 +0300

    Fix: adding refresh logic, refactor code

--- client/src/services/authentication/index.ts
+++ client/src/services/authentication/index.ts
@@ -10,28 +10,41 @@ class AuthenticationService {
         return this.instance;
     }
 
-    async logout(onSuccess?: (value?: any) => void, onError?: (error?: any) => void) {
+    async logout(onSuccess?: (value?: any) => void) {
         const url = `${appConfig.serverUrl}${appConfig.logoutEndpoint}`
         const res: any = await httpService.post(url);
         localStorage.setItem('user', '');
         if (typeof onSuccess === 'function') { onSuccess(res) }
     }
 
-    async refresh() {
+    async refresh(onSuccess?: (value: any) => void, onError?: () => void) {
         const url = `${appConfig.serverUrl}/api/refresh`
-        await httpService.get(url);
-    }
-
-    async login(data: { [key: string]: any }, onSuccess: (value: any) => void) {
-        const url = `${appConfig.serverUrl}${appConfig.loginEndpoint}`
-        const res: any = await httpService.post(url, JSON.stringify(data));
-        if (res.isAuthenticate) {
-            const userAsString = JSON.stringify(res.servicesRes.payload);
-            localStorage.setItem('user', userAsString);
-            onSuccess(res.servicesRes.payload);
+        const res: any = await httpService.get(url);
+        if (res?.isAuthenticate) {
+            this._onAuthenticationSuccess(res, onSuccess);
+        } else {
+            if (typeof onError === 'function') { onError(); }
         }
     }
 
+    async login(data: { [key: string]: any }, onSuccess: (value: any) => void, onError?: (value: string[]) => void) {
+        const url = `${appConfig.serverUrl}${appConfig.loginEndpoint}`
+        const res: any = await httpService.post(url, JSON.stringify(data));
+        if (res?.isAuthenticate) {
+            this._onAuthenticationSuccess(res, onSuccess);
+        } else {
+            if (typeof onError === 'function') {
+                onError(res.servicesRes.errors);
+            }
+        }
+    }
+
+
+    private _onAuthenticationSuccess(res: any, onSuccess?: (value: any) => void) {
+        const userAsString = JSON.stringify(res.servicesRes.payload);
+        localStorage.setItem('user', userAsString);
+        if (typeof onSuccess === 'function') { onSuccess(res.servicesRes.payload); }
+    }
 }
 
 export default AuthenticationService.getInstance()
