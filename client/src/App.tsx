ee4539fa96746bafe2307b3a31378345777d5473
amitvolga <amit.shimon@gmail.com>
2022-07-09 23:06:26 +0300

    Fix: remove user from state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,17 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 
 function App() {
-  const [user, setUser] = useState<{ [key: string]: any }>();
   return (
     <StoreProvider>
       <div className="app">
         <Routes>
           <Route path="*" element={<Navigate to={'home'} />} />
-          <Route path="login" element={<LoginForm setUser={setUser} />} />
+          <Route path="login" element={<LoginForm />} />
           <Route path="sign-up" element={<SignUpForm />} />
           <Route
             element={
-              <ProtectedRoute user={user}>
-                <Shell setUser={setUser} />
+              <ProtectedRoute >
+                <Shell />
               </ProtectedRoute>
             }
           >
