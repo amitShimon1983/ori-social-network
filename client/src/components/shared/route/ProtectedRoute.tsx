9d9131839bc6f7c2219b956958123678559f505c
amitvolga <amit.shimon@gmail.com>
2022-07-09 23:08:17 +0300

    Fix: remove user from props

--- client/src/components/shared/route/ProtectedRoute.tsx
+++ client/src/components/shared/route/ProtectedRoute.tsx
@@ -4,11 +4,10 @@ import { Navigate } from "react-router";
 import { appContextVar } from "../../../services/store";
 
 interface ProtectedRouteProps {
-    user: { [key: string]: any } | undefined,
     children: React.ReactNode;
 }
 
-const ProtectedRoute: FunctionComponent<ProtectedRouteProps> = ({ user, children }) => {
+const ProtectedRoute: FunctionComponent<ProtectedRouteProps> = ({  children }) => {
     const { isAuthenticate } = useReactiveVar(appContextVar);
     if (!isAuthenticate) {
         return <Navigate to="/login" replace />;
