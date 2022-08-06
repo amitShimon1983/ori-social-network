1a9e31e93fc8d118c6c4e0eb1d75040575e898db
amitvolga <amit.shimon@gmail.com>
2022-08-06 18:44:17 +0300

    ADD: file Service

---
+++ server/src/services/files/index.ts
@@ -0,0 +1,44 @@
+import PostModel from "../../model/schema/post/schema";
+import { IPost } from "../../model/schema/post/type";
+
+export class FileHandler {
+    private static instance: FileHandler;
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new FileHandler();
+        }
+        return this.instance;
+    }
+
+    async saveAll(files: Express.Multer.File[], user: any) {
+        console.log(files);
+        const newPosts = []
+        for (let index = 0; index < files?.length; index++) {
+            const file = files[index];
+            const newPost: IPost = {
+                encoding: file.encoding,
+                filename: file.fieldname,
+                mimetype: file.mimetype,
+                originalname: file.originalname,
+                path: file.path,
+                size: file.size,
+                userId: user._id
+            }
+            newPosts.push(newPost);
+        }
+        const posts = await PostModel.insertMany(newPosts);
+        console.log(posts);
+    }
+    // delete(fileName: string) {
+
+    // }
+    // get(id: string) {
+
+    // }
+    // getAll() {
+
+    // }
+}
+
+export default FileHandler.getInstance();
