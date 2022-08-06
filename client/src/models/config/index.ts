f4ee92d2e79f630a7a65e3d670cf3bf1d095c5c1
amitvolga <amit.shimon@gmail.com>
2022-08-06 18:41:38 +0300

    ADD: adding config prop

--- client/src/models/config/index.ts
+++ client/src/models/config/index.ts
@@ -4,11 +4,13 @@ export class Configuration {
     loginEndpoint?: string;
     logoutEndpoint?: string;
     uploadEndpoint?: string;
+    refreshEndpoint?: string;
     constructor() {
         this.serverUrl = process.env.REACT_APP_SERVER_URL;
         this.signUpEndpoint = process.env.REACT_APP_SIGN_UP;
         this.loginEndpoint = process.env.REACT_APP_LOGIN;
         this.logoutEndpoint = process.env.REACT_APP_LOGOUT;
-        this.logoutEndpoint = process.env.REACT_APP_UPLOAD;
+        this.uploadEndpoint = process.env.REACT_APP_UPLOAD;
+        this.refreshEndpoint = process.env.REACT_APP_REFRESH;
     }
 }
