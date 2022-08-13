394d84eb24f3ec07ce10c35cac8d32baadef4862
amitvolga <amit.shimon@gmail.com>
2022-08-13 19:54:23 +0300

    ADD: creating like type

--- server/src/model/schema/like/schema.ts
+++ server/src/model/schema/like/schema.ts
@@ -1,8 +1,9 @@
 import { model, Schema } from "mongoose";
-import { ILike } from "./type";
+import { ILike } from "./types";
 
 const likeSchema = new Schema<ILike>({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
+    post: { type: Schema.Types.ObjectId, ref: 'Post' },
 });
 
 const LikeModel = model<ILike>('Like', likeSchema);
---
+++ server/src/model/schema/like/types.ts
@@ -0,0 +1,4 @@
+export interface ILike {
+    user?: string;
+    post?: string;
+}
