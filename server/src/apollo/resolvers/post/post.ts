06443ffa02bafc235172bea75fb84ab25ed1b9f4
amitvolga <amit.shimon@gmail.com>
2022-08-13 19:56:43 +0300

    Fix: delete like post mutation

--- server/src/apollo/resolvers/post/post.ts
+++ server/src/apollo/resolvers/post/post.ts
@@ -9,9 +9,4 @@ export class PostResolver {
         const { user } = context;
         return postService.getRandomPosts(user._id) as Posts
     }
-    @Mutation(() => Posts)
-    async postLike(@Ctx() context: any): Promise<Posts> {
-        const { user } = context;
-        return postService.getRandomPosts(user._id) as Posts
-    }
 }
