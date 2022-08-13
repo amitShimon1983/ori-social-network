80cdac25f4b2e66a4b2aefca2703a27708cee3d7
amitvolga <amit.shimon@gmail.com>
2022-08-13 22:50:34 +0300

    Fix: refactor comment resolver

--- server/src/apollo/resolvers/comment/comment.ts
+++ server/src/apollo/resolvers/comment/comment.ts
@@ -1,19 +1,20 @@
-import { Mutation, Resolver, Query, Arg } from "type-graphql";
-import { Comment, Comments } from "./types";
+import { Mutation, Resolver, Query, Arg, Ctx } from "type-graphql";
+import { commentService } from "../../../services";
+import { Comment, Comments, CommentPostArgs, GetPostCommentsArgs } from "./types";
 
 @Resolver()
 export class CommentResolver {
     @Mutation(() => Comment)
-    async commentPost(@Arg('args', () => LikePostArgs) args: LikePostArgs, @Ctx() context: any): Promise<Like | null | undefined> {
+    async commentPost(@Arg('args', () => CommentPostArgs) args: CommentPostArgs, @Ctx() context: any): Promise<Comment | null | undefined> {
         const { user } = context;
-        if (args.postId && user._id && args.action) {
-            return likeService.likePost(args.postId, user._id, args.action) as Like;
+        if (args.postId && user._id && args?.content) {
+            return commentService.commentPost(user._id, args.postId,  args.content, args.commentId)
         }
     }
     @Query(() => Comments)
-    async getComments(@Arg('args', () => GetPostLikesArgs) args: GetPostLikesArgs): Promise<Likes | undefined> {
+    async getComments(@Arg('args', () => GetPostCommentsArgs) args: GetPostCommentsArgs): Promise<Comments | undefined> {
         if (args.postId) {
-            return likeService.getPostLikes(args.postId) as Likes;
+            return commentService.getPostComments(args.postId) as Comments;
         }
         return undefined;
     }
