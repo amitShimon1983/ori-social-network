6fab0d50dcc3e187a99acc536a6b2dedd7e11e5a
amitvolga <amit.shimon@gmail.com>
2022-08-13 23:23:07 +0300

    ADD: adding hook to get post comments

---
+++ client/src/hooks/useGetPostComments.tsx
@@ -0,0 +1,23 @@
+import { gql, useQuery } from "@apollo/client"
+const GET_POST_COMMENTS = gql`
+   query GetComments($postId: String, $commentId: String){
+    getComments(args:{ postId: $postId, $commentId: commentId}){
+        comments{
+            _id
+            user
+            content
+            createdAt
+            updatedAt
+        }
+    }
+   }
+`;
+export const useGetPostComments = (postId: string) => {
+    const { data, error, loading, fetchMore } = useQuery(GET_POST_COMMENTS, {
+        variables: {
+            postId
+        },
+        skip: !postId
+    })
+    return { data, error, loading, fetchMore }
+}
