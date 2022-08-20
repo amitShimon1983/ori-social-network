a61a501eb5a70405f1f6caa30854446311b05787
amitvolga <amit.shimon@gmail.com>
2022-08-20 16:43:03 +0300

    Add: get and save comment, UI

--- client/src/components/comments/Comments.tsx
+++ client/src/components/comments/Comments.tsx
@@ -1,7 +1,7 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import { useGetPostComments } from "../../hooks";
-import { Button, Spinner, Tree } from "../shared";
+import { useCommentPost, useGetPostComments } from "../../hooks";
+import { Button, Input, Spinner, Tree } from "../shared";
 
 interface CommentsThreadProps {
 
@@ -10,12 +10,39 @@ interface CommentsThreadProps {
 const CommentsThread: FunctionComponent<CommentsThreadProps> = () => {
     const { postId } = useParams();
     const navigate = useNavigate();
+    const [commentContent, setCommentContent] = useState<string>('')
+    const { commentPostMutation } = useCommentPost();
     const { data, error, loading, fetchMore } = useGetPostComments(postId || '');
     console.log({ postId, data, error });
+    const handleCommentSave = (data: any) => {
+        commentPostMutation({
+            variables: {
+                postId: postId,
+                content: commentContent,
+                commentId: data?.commentId
+            }
+        })
+    }
+    const handleCommentContentChange = (event: any) => {
+        const value = event.target.value;
+        setCommentContent(value)
+    }
     return (<>
         <Button handleClick={() => navigate(-1)}>Back</Button>
         {loading && <Spinner label="loading..." />}
-        <Tree data={[]} />
+        <Tree data={[]} fetchMore={fetchMore} >
+            <Input handleChange={handleCommentContentChange}
+                type={'text'}
+                name={'comment'}
+                className={'comment'}
+                required={true}
+                placeholder={'write a comment...'}
+                value={commentContent}
+            />
+            <Button disabled={!commentContent} handleClick={handleCommentSave}>
+                Save comment
+            </Button>
+        </Tree>
     </>);
 }
 
