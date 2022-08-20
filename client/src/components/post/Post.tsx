00d357dfc3e6bc0422a52bbcd7d3dfaa2094435e
amitvolga <amit.shimon@gmail.com>
2022-08-20 18:29:28 +0300

    Fix: set comments number post view

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -6,17 +6,18 @@ import { httpService } from "../../services";
 import { PostDetails } from "./types";
 import classes from './Post.module.css';
 import { appContextVar } from "../../services/store";
-import { useGetPostLikes } from "../../hooks";
+import { useGetPostComments, useGetPostLikes } from "../../hooks";
 interface PostProps {
     post: PostDetails;
     containerClassName?: string;
 }
 const filesUri = `${appConfig.serverUrl}${'/api/file/post/'}`
 const Post: FunctionComponent<PostProps> = ({ post, containerClassName }) => {
+    const { data: commentsData, } = useGetPostComments(post?._id || '');
     const { user } = appContextVar();
     const [url, setUrl] = useState<string>('');
     const [type, setType] = useState<string>('');
-    const { data } = useGetPostLikes(post?._id || '');
+    const { data: likesData } = useGetPostLikes(post?._id || '');
 
     useEffect(() => {
         const loadFile = async () => {
@@ -31,7 +32,11 @@ const Post: FunctionComponent<PostProps> = ({ post, containerClassName }) => {
     return (<div className={`${classes.container} ${containerClassName}`}>
         {url && isVideo && <Video type={type} link={url} />}
         {url && !isVideo && <img className={classes.image} src={url} alt={'post'} />}
-        {!!post?._id && <VideoFooter postId={post?._id} me={user} likes={data?.getLikes?.likes} />}
+        {!!post?._id && <VideoFooter
+            postId={post?._id}
+            me={user}
+            likes={likesData?.getLikes?.likes}
+            comments={commentsData?.getComments?.comments} />}
     </div>);
 }
 
