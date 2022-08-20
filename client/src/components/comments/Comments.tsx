66b51a044f1eb14c140f265bebf1a4d66dce82b8
amitvolga <amit.shimon@gmail.com>
2022-08-20 18:29:04 +0300

    Fix: adding fetch comments children

--- client/src/components/comments/Comments.tsx
+++ client/src/components/comments/Comments.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { useCommentPost, useGetPostComments } from "../../hooks";
 import { Button, Input, Spinner, Tree } from "../shared";
-
+import classes from './Comment.module.css';
 interface CommentsThreadProps {
 
 }
@@ -13,7 +13,6 @@ const CommentsThread: FunctionComponent<CommentsThreadProps> = () => {
     const [commentContent, setCommentContent] = useState<string>('')
     const { commentPostMutation } = useCommentPost();
     const { data, error, loading, fetchMore } = useGetPostComments(postId || '');
-    console.log({ postId, data, error });
     const handleCommentSave = (data: any) => {
         commentPostMutation({
             variables: {
@@ -27,23 +26,36 @@ const CommentsThread: FunctionComponent<CommentsThreadProps> = () => {
         const value = event.target.value;
         setCommentContent(value)
     }
-    return (<>
-        <Button handleClick={() => navigate(-1)}>Back</Button>
-        {loading && <Spinner label="loading..." />}
-        <Tree data={[]} fetchMore={fetchMore} >
-            <Input handleChange={handleCommentContentChange}
-                type={'text'}
-                name={'comment'}
-                className={'comment'}
-                required={true}
-                placeholder={'write a comment...'}
-                value={commentContent}
-            />
-            <Button disabled={!commentContent} handleClick={handleCommentSave}>
-                Save comment
-            </Button>
-        </Tree>
-    </>);
+
+    const handleFetchChildren = async (data: any) => {
+        const { data: fetchData } = await fetchMore({
+            variables: {
+                postId,
+                commentId: data?.commentId
+            },
+        });
+        return fetchData?.getComments?.comments || []
+    }
+    return (
+        <div className={classes.container}>
+            <Button handleClick={() => navigate(-1)}>Back</Button>
+            {loading && <Spinner label="loading..." />}
+            <Tree styles={{ treeClassName: classes.comments }} data={data?.getComments?.comments || []} fetchMore={handleFetchChildren} />
+            <div className={classes.footer}>
+                <Input handleChange={handleCommentContentChange}
+                    type={'text'}
+                    name={'comment'}
+                    className={''}
+                    required={true}
+                    placeholder={'write a comment...'}
+                    value={commentContent}
+                />
+                <Button disabled={!commentContent} handleClick={handleCommentSave}>
+                    Save comment
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default CommentsThread;
