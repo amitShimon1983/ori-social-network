3004764fe84f3937df9fed23dc6291d0b5a8e51c
amitvolga <amit.shimon@gmail.com>
2022-08-20 16:18:58 +0300

    ADD: hook to post comment

---
+++ client/src/hooks/useCommentPost.tsx
@@ -0,0 +1,13 @@
+import { gql, useMutation } from "@apollo/client"
+
+const COMMENT_POST = gql`
+mutation CommentPost($postId: String, $content: String, $commentId: String){
+    commentPost(args: { postId: $postId, content: $content, commentId: $commentId }){
+        _id
+    }
+}
+`
+export const useCommentPost = () => {
+    const [commentPostMutation, { data, loading, error }] = useMutation(COMMENT_POST);
+    return { commentPostMutation, data, loading, error }
+}
