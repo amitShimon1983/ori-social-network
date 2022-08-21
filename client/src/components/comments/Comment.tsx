b0d108ac252ce47d676cf908a41f1f8b4937aea6
amitvolga <amit.shimon@gmail.com>
2022-08-21 20:26:24 +0300

    ADD: adding setChild callback

--- client/src/components/comments/Comment.tsx
+++ client/src/components/comments/Comment.tsx
@@ -5,19 +5,28 @@ interface CommentProps {
     content: string;
     createdAt: string;
     user: string;
+    setReplyTo: (id: any) => void;
+    setChild: (data: any[]) => void;
 }
 
 const Comment: FunctionComponent<CommentProps> = ({ _id,
+    setReplyTo,
+    setChild,
     content,
     createdAt,
     user }) => {
-    const date = new Date(+createdAt)
+    const date = new Date(+createdAt);
+
     return (<>
         <div style={{ display: 'flex' }}>
             <div>{user}&nbsp;</div>
             <div> {date.toLocaleDateString()}</div>
         </div>
         <div>{content}</div>
+        <div onClick={(event) => {
+            event.stopPropagation();
+            setReplyTo({ commentId: _id, setChild });
+        }}>Reply To</div>
     </>);
 }
 
