4243bdd019280771a2225a9a44ff840e4326876e
amitvolga <amit.shimon@gmail.com>
2022-08-21 22:23:35 +0300

    ADD: adding me component

--- client/src/components/comments/Comment.tsx
+++ client/src/components/comments/Comment.tsx
@@ -1,33 +1,28 @@
 import { FunctionComponent } from "react";
-
+import Me from "../me";
+import classes from './Comment.module.css';
 interface CommentProps {
     _id: string;
     content: string;
     createdAt: string;
-    user: string;
-    setReplyTo: (id: any) => void;
-    setChild: (data: any[]) => void;
+    user: any;
 }
 
 const Comment: FunctionComponent<CommentProps> = ({ _id,
-    setReplyTo,
-    setChild,
     content,
     createdAt,
     user }) => {
     const date = new Date(+createdAt);
 
-    return (<>
-        <div style={{ display: 'flex' }}>
-            <div>{user}&nbsp;</div>
-            <div> {date.toLocaleDateString()}</div>
+    return (<div className={classes.comment_container}>
+        <div className={classes.comment_header}>
+            <div>
+                <Me imageClass={classes.image} displayDetails={false} user={user} />
+            </div>
+            <div className={classes.date}> {date.toLocaleDateString()}</div>
         </div>
         <div>{content}</div>
-        <div onClick={(event) => {
-            event.stopPropagation();
-            setReplyTo({ commentId: _id, setChild });
-        }}>Reply To</div>
-    </>);
+    </div>);
 }
 
 export default Comment;
