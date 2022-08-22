2fa2016b240108a941d4d0c5d1214151bc8fef11
amitvolga <amit.shimon@gmail.com>
2022-08-22 23:38:03 +0300

    ADD: adding read more read less in case of text overflow

--- client/src/components/comments/Comment.tsx
+++ client/src/components/comments/Comment.tsx
@@ -1,6 +1,8 @@
-import { FunctionComponent } from "react";
-import { getPostDate } from "../../services/data";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+import { getPostDate } from "../../services/date";
+import { isOverflow } from "../../utils";
 import Me from "../me";
+import { Button } from "../shared";
 import classes from './Comment.module.css';
 interface CommentProps {
     _id: string;
@@ -13,8 +15,21 @@ const Comment: FunctionComponent<CommentProps> = ({ _id,
     content,
     createdAt,
     user }) => {
+
+    const [isContentOverflow, setIsContentOverFlow] = useState<boolean>(false);
+    const [toggle, setToggle] = useState<boolean>(false);
+    let contentRef = useRef(null);
     const date = new Date(+createdAt);
     const diff = getPostDate(date)
+
+    useEffect(() => {
+        if (contentRef?.current && isOverflow(contentRef.current)) {
+            setIsContentOverFlow(true)
+        }
+    }, [contentRef])
+
+    const toggleReadMore = () => setToggle(prev => !prev);
+  
     return (<div className={classes.comment_container}>
         <div className={classes.comment_header}>
             <div className={classes.comment_details}>
@@ -25,14 +40,16 @@ const Comment: FunctionComponent<CommentProps> = ({ _id,
                             containerClassName: classes.me_container,
                             emailClassName: classes.me_email
                         }}
-                        displayDetails={false}
                         user={user}
                     />
                 </div>
                 <div className={classes.date}> {diff}</div>
             </div>
         </div>
-        <div className={classes.content}>{content}</div>
+        <div ref={contentRef} className={`${classes.content} ${toggle && classes.content_overflow}`}>{content}</div>
+        {isContentOverflow && <div className={classes.read_more_button_container}>
+            <Button className={classes.read_more_button} handleClick={toggleReadMore}>read {toggle ? 'less...' : 'More...'}</Button>
+        </div>}
     </div>);
 }
 
