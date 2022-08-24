63b127120846a0dbe47a36061a5172f818a34240
amitvolga <amit.shimon@gmail.com>
2022-08-24 23:01:13 +0300

    Fix: passing posts as props

--- client/src/components/post/PostList.tsx
+++ client/src/components/post/PostList.tsx
@@ -1,39 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
 import { FunctionComponent } from "react";
 import { Spinner } from "../shared";
 import Post from "./Post";
 import { PostDetails } from "./types";
-const GET_RANDOM_POSTS = gql`
-query GetRandomPosts{
-    getRandomPosts{
-        posts{
-            _id
-            user
-            title
-            createdAt
-            file {
-                originalname
-                encoding
-                mimetype
-                filename
-                path
-                size
-            }
-        }
-    }
-}
-`
 interface PostListProps {
     postClassName?: string;
+    posts: any[];
 }
 
-const PostList: FunctionComponent<PostListProps> = ({ postClassName }) => {
-    const { data, error, loading } = useQuery(GET_RANDOM_POSTS)
+const PostList: FunctionComponent<PostListProps> = ({ posts, postClassName }) => {
     return (<>
-        {loading && <Spinner label="Loading..." />}
-        {error && <div>{error?.message}</div>}
-        {!!data?.getRandomPosts.posts?.length &&
-            data?.getRandomPosts?.posts?.map((post: PostDetails) => <Post containerClassName={postClassName}key={post._id} post={post} />)}
+        {!!posts?.length &&
+            posts?.map((post: PostDetails) => <Post containerClassName={postClassName} key={post._id} post={post} />)}
     </>);
 }
 
