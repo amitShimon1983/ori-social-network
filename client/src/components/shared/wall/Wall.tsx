9d49d8fcee9cd2d0f62ca117d4251bc008752696
amitvolga <amit.shimon@gmail.com>
2022-08-30 20:43:25 +0300

    ADD: adding followers to wall

--- client/src/components/shared/wall/Wall.tsx
+++ client/src/components/shared/wall/Wall.tsx
@@ -1,5 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
+import useFollow from '../../../hooks/useFollow';
+import { appContextVar } from '../../../services/store';
 import Me from '../../me';
 import PostList from '../../post/PostList';
 import classes from './Wall.module.css';
@@ -22,14 +24,51 @@ query GetMyPosts($userId: String){
                 path
                 size
             }
+            
         }
     }
 }
 `
 const Wall: FunctionComponent<WallProps> = ({ user }) => {
+    const [userState, setUserState] = useState<any>();
+    useEffect(() => {
+        setUserState(user)
+    }, [user])
+
     const { data, loading } = useQuery(GET_My_POSTS, { variables: { userId: user._id } })
+    const { user: me } = appContextVar();
+    const { followMutation } = useFollow()
+    const isMe = me._id === user._id
+
+    const isFollowedByMe = !!me?.following?.find((follower: any) => {
+        console.log({ isMe, follower, me: user._id });
+        return follower === user._id
+    });
+    
+    console.log({ isFollowedByMe });
+    const handleFollow = async () => {
+        followMutation({
+            variables: {
+                userId: user._id
+            }
+        })
+        appContextVar({
+            ...me,
+            following: [...me.following, user._id]
+        });
+    }
     return (<>
         <Me styles={{ imageClass: classes.image }} user={user} />
+        <div className={`${classes.following_container}`}>
+            <div className={`${classes.following}`}>
+                followers {user?.followers?.length || 0}
+            </div>
+            <div className={`${classes.following}`}>
+                following {user?.following?.length || 0}
+            </div>
+        </div>
+        {!isMe && !isFollowedByMe &&
+            <div onClick={handleFollow} className={`${classes.following_link}`}>Follow me...</div>}
         <div className={classes.container}>
             {!loading && <PostList posts={data?.getMyPosts?.posts} postClassName={classes.post} />}
         </div>
