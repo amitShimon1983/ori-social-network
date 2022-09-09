26496877d4bc7ed026bff360524927097fb06eb5
amitvolga <amit.shimon@gmail.com>
2022-09-09 15:36:39 +0300

    Fix: adding spinner

--- client/src/components/me/index.tsx
+++ client/src/components/me/index.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { appConfig } from "../../configuration";
 import { httpService } from "../../services";
+import { Spinner } from "../shared";
 import { Video } from "../video";
 import classes from './Me.module.css';
 
@@ -13,14 +14,17 @@ const filesUri = `${appConfig.serverUrl}${'/api/file/post/'}`
 const Me: FunctionComponent<MeProps> = ({ user, styles }) => {
     const [url, setUrl] = useState<string>('');
     const [type, setType] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
     const navigate = useNavigate();
     const handleNavigateToUser = () => navigate(`/other/${user._id}`);
     useEffect(() => {
+        setLoading(true);
         const loadFile = async () => {
             const blob: any = await httpService.getStream(filesUri + user.file.originalname);
             const objectURL = URL.createObjectURL(blob);
             setType(blob?.type)
             setUrl(objectURL)
+            setLoading(false);
         }
         loadFile();
     }, [])
@@ -31,6 +35,7 @@ const Me: FunctionComponent<MeProps> = ({ user, styles }) => {
                 <Video videoClassName={classes.video} type={type} link={url} />
             </div>
             }
+            {loading && <Spinner label={'Loading'} />}
             {url && !isVideo && <img className={styles?.imageClass} src={url} alt={'profilePicture'} />}
             {<p onClick={handleNavigateToUser} className={`${classes.email} ${styles?.emailClassName}`}>{user?.email}</p>}
         </div>
