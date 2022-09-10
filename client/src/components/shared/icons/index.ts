256dfcd380cdb62171b3b89c31861b4876c0a9d0
amitvolga <amit.shimon@gmail.com>
2022-09-10 20:04:58 +0300

    Fix: adding more imports

--- client/src/components/shared/icons/index.ts
+++ client/src/components/shared/icons/index.ts
@@ -1,9 +1,10 @@
-export { AiFillBackward, AiOutlineLogin, AiOutlineLogout, AiOutlineCloudUpload } from 'react-icons/ai';
+export { AiOutlineClose, AiFillBackward, AiOutlineLogin, AiOutlineLogout, AiOutlineCloudUpload } from 'react-icons/ai';
 export { FiRefreshCw, FiVideoOff, FiVideo, FiInbox } from 'react-icons/fi';
 export { FcHome, FcLike } from 'react-icons/fc';
-export { FaRegComments, } from 'react-icons/fa';
+export { FaRegComments, FaPencilAlt } from 'react-icons/fa';
 export { CgProfile } from 'react-icons/cg';
 export { BiVideoRecording, BiLike, BiImage } from 'react-icons/bi';
 export { GiArrowDunk } from 'react-icons/gi';
 export { BsCloudDownload, BsSave } from 'react-icons/bs';
 export { RiDeleteBin2Fill } from 'react-icons/ri';
+export { ImFilesEmpty } from 'react-icons/im';
