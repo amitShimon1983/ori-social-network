75435c4967d2a1ecec98f1289fe21d924b2561cf
amitvolga <amit.shimon@gmail.com>
2022-09-10 17:27:16 +0300

    ADD: types message resolve

--- server/src/apollo/resolvers/message/types.ts
+++ server/src/apollo/resolvers/message/types.ts
@@ -1,10 +1,21 @@
-import { ObjectType, Field } from "type-graphql";
+import { ObjectType, Int, Field, InputType } from "type-graphql";
 import { IMessage } from "../../../model/schema/message";
 
 @ObjectType()
 export class GetMessages {
     @Field(() => [Message], { nullable: true })
-    messages: Message[];
+    threads: Message[];
+    @Field(() => Int, { nullable: true })
+    count: number;
+    @Field(() => Boolean, { nullable: true })
+    hasMore: boolean;
+}
+@InputType()
+export class GetMessagesArgs {
+    @Field(() => Int, { nullable: true })
+    skip?: number;
+    @Field(() => Int, { nullable: true })
+    limit?: number;
 }
 
 export class Message implements IMessage {
