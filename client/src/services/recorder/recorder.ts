bf5f5963f8dc7f1c29691ba17405bea5a856a391
amitvolga <amit.shimon@gmail.com>
2022-09-24 18:22:46 +0300

    Fix: rename properties

--- client/src/services/recorder/recorder.ts
+++ client/src/services/recorder/recorder.ts
@@ -1,55 +1,55 @@
 export class Recorder {
-    cameraStream?: MediaStream;
-    mediaRecorder?: MediaRecorder;
-    recordedBlobs: any[];
-    recordedBlob?: Blob;
+    _mediaStream?: MediaStream;
+    _mediaRecorder?: MediaRecorder;
+    _recordedBlobs: any[];
+    _recordedBlob?: Blob;
     onRecordingStopHandler?: (blob: Blob) => void;
     onDataAvailableHandler?: (event: any) => void;
-    constructor(camera_stream: MediaStream, onDataAvailable?: (event: any) => void, onRecordingStop?: (blob: Blob) => void) {
-        this.cameraStream = camera_stream;
-        this.recordedBlobs = [];
+    constructor(mediaStream: MediaStream, onDataAvailable?: (event: any) => void, onRecordingStop?: (blob: Blob) => void) {
+        this._mediaStream = mediaStream;
+        this._recordedBlobs = [];
         this.onDataAvailableHandler = onDataAvailable;
         this.onRecordingStopHandler = onRecordingStop;
     }
 
     start() {
 
-        if (this?.cameraStream) {
-            this.mediaRecorder = new MediaRecorder(this?.cameraStream!, {
+        if (this?._mediaStream) {
+            this._mediaRecorder = new MediaRecorder(this?._mediaStream!, {
                 mimeType: 'video/webm',
                 audioBitsPerSecond: 128000,
                 videoBitsPerSecond: 2500000,
             });
-            this.mediaRecorder.ondataavailable = this._onDataAvailable.bind(this);
-            this.mediaRecorder.onstop = this._onRecordingStop.bind(this);
-            this.mediaRecorder.start(1000);
+            this._mediaRecorder.ondataavailable = this._onDataAvailable.bind(this);
+            this._mediaRecorder.onstop = this._onRecordingStop.bind(this);
+            this._mediaRecorder.start(1000);
         }
     }
 
     stop() {
-        if (this.mediaRecorder) {
-            this.mediaRecorder?.stop()
+        if (this._mediaRecorder) {
+            this._mediaRecorder?.stop()
         }
     }
 
     _onRecordingStop() {
-        this.recordedBlob = new Blob(this.recordedBlobs, { type: 'video/webm' });
+        this._recordedBlob = new Blob(this._recordedBlobs, { type: 'video/webm' });
         if (typeof this.onRecordingStopHandler === 'function') {
-            this.onRecordingStopHandler(this.recordedBlob)
+            this.onRecordingStopHandler(this._recordedBlob)
         }
-        this.mediaRecorder = undefined;
-        this.cameraStream = undefined;
-        this.recordedBlobs = []
+        this._mediaRecorder = undefined;
+        this._mediaStream = undefined;
+        this._recordedBlobs = []
     }
 
     _onDataAvailable(event: any) {
-        this.recordedBlobs.push(event.data);
+        this._recordedBlobs.push(event.data);
         if (typeof this.onDataAvailableHandler === 'function') {
             this.onDataAvailableHandler(event)
         }
     }
     getRecordingResults() {
-        return this.recordedBlob;
+        return this._recordedBlob;
     }
 
 }
