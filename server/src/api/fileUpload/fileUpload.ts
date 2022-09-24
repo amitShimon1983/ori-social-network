e632b56bd788d07e80468618393b0bc9d9a767df
amitvolga <amit.shimon@gmail.com>
2022-09-24 23:43:15 +0300

    ADD: upload voice message

--- server/src/api/fileUpload/fileUpload.ts
+++ server/src/api/fileUpload/fileUpload.ts
@@ -1,17 +1,38 @@
 import { Router, Request, Response } from 'express';
 import path from 'path';
-import { fileService, jwtService, postService } from '../../services';
+import { fileService, jwtService, messageService, postService } from '../../services';
 import { upload } from '../../app/middleware/uploadMiddleware';
 const router = Router();
 
-router.post('/upload', upload.array("files"), async (req: Request, res: Response) => {
+router.post('/uploadPost', upload.array("files"), async (req: Request, res: Response) => {
     let response;
     if (req.files) {
         const files = req.files
         const user = jwtService.getCookieData(req.cookies.user);
         if (user) {
             const dbFiles = await fileService.saveAll(files as Express.Multer.File[]);
-            await postService.savePosts(dbFiles, user)
+            await postService.savePosts(dbFiles, user);
+        }
+    }
+    res.status(200).json(response)
+})
+router.post('/uploadMessage', upload.array("files"), async (req: Request, res: Response) => {
+    let response;
+    if (req.files) {
+        const files = req.files
+        const user = jwtService.getCookieData(req.cookies.user);
+        if (user) {
+            const dbFiles = await fileService.saveAll(files as Express.Multer.File[]);
+            const messageArgs = {
+                ...req.body,
+                type: 'audio',
+                content: 'Audio',
+            }
+            const dbMessage = await messageService.sendMessage(messageArgs, user._id);
+            if (dbMessage?._id) {
+                dbMessage.file = dbFiles[0]._id;
+                await dbMessage.save();
+            }
         }
     }
     res.status(200).json(response)
