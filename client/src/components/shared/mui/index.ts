3d4287ea51582c5353b0f89f0b79d69584eef725
amitvolga <amit.shimon@gmail.com>
2022-09-24 21:20:30 +0300

    Fix: import

--- client/src/components/shared/mui/index.ts
+++ client/src/components/shared/mui/index.ts
@@ -1,4 +1,4 @@
-export { Paper, Autocomplete, CircularProgress, Fab, IconButton } from '@mui/material';
+export { Paper, Autocomplete, CircularProgress, Fab, IconButton, CardMedia } from '@mui/material';
 export { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 export { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 export { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
