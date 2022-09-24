c123e36f3033d12177fad11650b476d90c0af6f6
amitvolga <amit.shimon@gmail.com>
2022-09-24 20:01:15 +0300

    Fix: change upload name

--- client/src/models/config/index.ts
+++ client/src/models/config/index.ts
@@ -3,14 +3,16 @@ export class Configuration {
     signUpEndpoint?: string;
     loginEndpoint?: string;
     logoutEndpoint?: string;
-    uploadEndpoint?: string;
+    uploadPostEndpoint?: string;
+    uploadMessageEndpoint?: string;
     refreshEndpoint?: string;
     constructor() {
         this.serverUrl = process.env.REACT_APP_SERVER_URL;
         this.signUpEndpoint = process.env.REACT_APP_SIGN_UP;
         this.loginEndpoint = process.env.REACT_APP_LOGIN;
         this.logoutEndpoint = process.env.REACT_APP_LOGOUT;
-        this.uploadEndpoint = process.env.REACT_APP_UPLOAD;
+        this.uploadMessageEndpoint = process.env.REACT_APP_UPLOAD_MESSAGE;
+        this.uploadPostEndpoint = process.env.REACT_APP_UPLOAD_POST;
         this.refreshEndpoint = process.env.REACT_APP_REFRESH;
     }
 }
