3d4c0f23a2c6af4f702714677c5a84da640b1dd9
amitvolga <amit.shimon@gmail.com>
2022-09-24 21:17:51 +0300

    Fix: adding more icons

--- client/src/components/shared/icons/index.ts
+++ client/src/components/shared/icons/index.ts
@@ -1,7 +1,7 @@
 export { AiOutlineClose, AiFillBackward, AiOutlineLogin, AiOutlineCamera, AiOutlineLogout, AiOutlineCloudUpload } from 'react-icons/ai';
 export { FiRefreshCw, FiVideoOff, FiVideo, FiInbox } from 'react-icons/fi';
 export { FcHome, FcLike } from 'react-icons/fc';
-export { FaRegComments, FaPencilAlt, FaMicrophoneAlt } from 'react-icons/fa';
+export { FaRegComments, FaPencilAlt, FaMicrophoneAlt, FaMicrophoneAltSlash } from 'react-icons/fa';
 export { CgProfile } from 'react-icons/cg';
 export { BiVideoRecording, BiLike, BiImage } from 'react-icons/bi';
 export { GiArrowDunk } from 'react-icons/gi';
