8e4466888c941bcb3a9bc5277cb174e65acfbdcf
amitvolga <amit.shimon@gmail.com>
2022-09-27 03:31:04 +0300

    Fix: moving ref scrolling into list, adding skeleton when rendering items

--- client/src/components/shared/speechBubble/components/SpeechBubbleList.tsx
+++ client/src/components/shared/speechBubble/components/SpeechBubbleList.tsx
@@ -1,6 +1,7 @@
-import {  FunctionComponent, useEffect, useRef } from "react";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
 import SpeechBubble from "./SpeechBubble";
 import classes from './index.module.css';
+import Skeleton from "../../skeleton";
 interface SpeechBubbleListProps {
     items: any[];
     onItemClick: (item: any) => void | Promise<void>
@@ -8,18 +9,37 @@ interface SpeechBubbleListProps {
 
 const SpeechBubbleList: FunctionComponent<SpeechBubbleListProps> = ({ items, onItemClick }) => {
     let innerRef = useRef<HTMLSpanElement>(null);
+    const [displaySkeleton, setDisplaySkeleton] = useState<boolean>(true);
     useEffect(() => {
-        setTimeout(() => {
-            innerRef?.current?.scrollIntoView({ behavior: 'auto', block: 'end', inline: 'end' });
-        }, 600)
-    }, [items?.length])
+        if (displaySkeleton) {
+            let timer = setTimeout(() => {
+                setDisplaySkeleton(false)
+                innerRef?.current?.scrollIntoView({
+                    block: "end",
+                    inline: "end",
+                    behavior: "smooth"
+                });
+            }, 1500);
+            return () => {
+                return clearTimeout(timer)
+            }
+        } else {
+            innerRef?.current?.scrollIntoView({
+                block: "end",
+                inline: "end",
+                behavior: "smooth"
+            });
+        }
+    }, [items?.length, displaySkeleton])
     return (<>
-        {items?.map((item: any, idx: number) => {
+
+        {items.map((item: any, idx: number) => {
             const isLast = idx === items?.length - 1;
             return <span
                 className={classes.speech_container}
                 key={`SpeechBubble_${item._id}_item_Form_ref`}
             >
+                {displaySkeleton && <Skeleton />}
                 <SpeechBubble
                     onClickHandler={onItemClick}
                     key={`SpeechBubble_${item._id}_item_Form`}
@@ -27,7 +47,8 @@ const SpeechBubbleList: FunctionComponent<SpeechBubbleListProps> = ({ items, onI
                 />
                 {isLast && <span ref={innerRef}>last</span>}
             </span>
-        })}</>);
+        })}
+    </>);
 }
 
 export default SpeechBubbleList;
