77693e1e3bce8528687751bf212643a5bab35801
amitvolga <amit.shimon@gmail.com>
2022-10-07 23:56:21 +0300

    Fix: refactoring apollo server to support ws

--- server/src/apollo/apolloContext.ts
+++ server/src/apollo/apolloContext.ts
@@ -1,16 +1,28 @@
 import { AuthenticationError } from 'apollo-server-errors';
 import { jwtService } from '../services';
+import { PubSub } from 'graphql-subscriptions';
 
-export const apolloContext = () => ({ req, res, event }: { req: any; res: any; event: any; }) => {
+export const apolloHttpContext = (pubSub: PubSub) => ({ req, }: { req: any; }) => {
     if (req.body.operationName === 'IntrospectionQuery') {
         return {}
     }
+    const token = req?.cookies?.user;
+    return authenticateRequest(token, pubSub);
+}
+export const apolloWsContext = (pubSub: PubSub, ctx: any, msg: any, args: any) => {
+    const req = ctx.extra.request;
+    const cookie = req.rawHeaders.find((header: string) => header.includes('user='));
+    return authenticateRequest(cookie?.split?.('=')?.[1], pubSub);
+};
 
-    if (!req?.cookies?.user) {
+function authenticateRequest(token: any, pubSub: PubSub) {
+    if (!token) {
         throw new AuthenticationError('UNAUTHENTICATED');
     }
-    const user = jwtService.verify(req?.cookies?.user);
+    const { data } = jwtService.verify(token);
+    const user = JSON.parse(data);
     return {
-        user: JSON.parse(user?.data)
-    }
+        user,
+        pubSub
+    };
 }
