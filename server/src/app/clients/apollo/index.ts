0e1ac761760e9495df961470b0da0cccd5e126d4
amitvolga <amit.shimon@gmail.com>
2022-10-07 23:59:32 +0300

    Fix: clean code

--- server/src/app/clients/apollo/index.ts
+++ server/src/app/clients/apollo/index.ts
@@ -9,55 +9,38 @@ import { useServer } from 'graphql-ws/lib/use/ws';
 import { Express } from 'express'
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { apolloContext } from '../../../apollo';
-import resolvers from './resolvers'
-import { UserModel } from '../../../model';
-const getDynamicContext = async (ctx: any, msg: any, args: any) => {
-    // ctx is the graphql-ws Context where connectionParams live
-    if (ctx.connectionParams.authentication) {
-        const currentUser = await UserModel.find({});
-        return { currentUser };
-    }
-    // Otherwise let our resolvers know we don't have a current user
-    return { currentUser: null };
-};
+import { apolloHttpContext, apolloWsContext } from '../../../apollo';
+import resolvers from './resolvers';
+import { PubSub } from 'graphql-subscriptions';
+const pubSub = new PubSub();
+
 const createApolloServer = async (app: Express) => {
 
     const schema = await buildSchema({
         resolvers: resolvers,
+        pubSub,
         validate: false,
         emitSchemaFile: true,
         nullableByDefault: true
     })
     const httpServer = createServer(app);
-    // Hand in the schema we just created and have the
-    // WebSocketServer start listening.
-    // Creating the WebSocket server
     const wsServer = new WebSocketServer({
-        // This is the `httpServer` we created in a previous step.
         server: httpServer,
-        // Pass a different path here if your ApolloServer serves at
-        // a different path.
         path: '/api/graphql',
     });
     const serverCleanup = useServer({
         schema,
         context: (ctx, msg, args) => {
-            // Returning an object will add that information to our
-            // GraphQL context, which all of our resolvers have access to.
-            return getDynamicContext(ctx, msg, args);
+            return apolloWsContext(pubSub, ctx, msg, args);
         },
     }, wsServer);
     const server = new ApolloServer({
-        context: apolloContext(),
+        context: apolloHttpContext(pubSub),
         schema: schema,
         csrfPrevention: true,
         cache: "bounded",
         plugins: [
-            // Proper shutdown for the HTTP server.
             ApolloServerPluginDrainHttpServer({ httpServer }),
-
-            // Proper shutdown for the WebSocket server.
             {
                 async serverWillStart() {
                     return {
@@ -72,6 +55,6 @@ const createApolloServer = async (app: Express) => {
     });
     await server.start()
     server.applyMiddleware({ app, path: '/api/graphql', cors: false })
-    return httpServer;
+    return { httpServer, pubSub };
 }
 export default createApolloServer;
