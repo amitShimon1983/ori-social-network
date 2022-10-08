74e0834de73b32a204580766bbfc924fdb314ad3
amitvolga <amit.shimon@gmail.com>
2022-10-08 20:22:08 +0300

    Fix: extract logic for authorization to middleware

--- server/src/apollo/apolloContext.ts
+++ server/src/apollo/apolloContext.ts
@@ -1,42 +1,20 @@
-import { AuthenticationError } from 'apollo-server-errors';
-import { authenticationService, cookieService, jwtService } from '../services';
 import { PubSub } from 'graphql-subscriptions';
 import { Request, Response } from 'express';
 import { SubscribeMessage } from 'graphql-ws';
 import { ExecutionArgs } from 'graphql';
+import { authenticateRequestMiddleware } from '../app/middleware/authenticationMiddleware';
 
 export const apolloHttpContext = (pubSub: PubSub) => async ({ req, res }: { req: Request; res: Response }) => {
     if (req.body.operationName === 'IntrospectionQuery') {
         return {}
     }
     const token = req?.cookies?.user;
-    return await authenticateRequest(token, pubSub, res);
+    const user = await authenticateRequestMiddleware(token, res);
+    return { user, pubSub }
 }
 export const apolloWsContext = async (pubSub: PubSub, ctx: any, msg: SubscribeMessage, args: ExecutionArgs) => {
     const req = ctx.extra.request;
     const cookie = req.rawHeaders.find((header: string) => header.includes('user='));
-    return await authenticateRequest(cookie?.split?.('=')?.[1], pubSub);
+    const user = await authenticateRequestMiddleware(cookie?.split?.('=')?.[1]);
+    return { user, pubSub }
 };
-
-async function authenticateRequest(token: string, pubSub: PubSub, res?: Response) {
-    if (!token) {
-        throw new AuthenticationError('UNAUTHENTICATED');
-    }
-    const { data, error } = jwtService.verify(token);
-    if (error === 'UNAUTHENTICATED') {
-        throw new AuthenticationError('UNAUTHENTICATED');
-    }
-    if (error === 'refresh_token') {
-        const { isAuthenticate, token: resToken } = await authenticationService.refresh(token);
-        if (isAuthenticate && res) {
-            cookieService.setCookie(res, resToken, 'user');
-        }
-    }
-    const user = JSON.parse(data);
-    return {
-        user,
-        pubSub
-    };
-}
-
-
