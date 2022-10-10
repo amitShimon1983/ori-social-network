12d213ec707fa867bc35b2d6fab90ed09a610dc8
amitvolga <amit.shimon@gmail.com>
2022-10-10 23:15:51 +0300

    ADD: adding onMessageUpdate query

--- client/src/queries/inboxQueries.ts
+++ client/src/queries/inboxQueries.ts
@@ -1,6 +1,14 @@
 import { gql } from "@apollo/client"
 import { CORE_MESSAGE_FIELDS, CORE_USER_FIELDS } from "../fragments"
 
+const ON_MESSAGE_UPDATE_SUBSCRIPTION = gql`
+${CORE_MESSAGE_FIELDS}
+${CORE_USER_FIELDS}
+subscription OnMessageUpdate {
+    onMessageUpdate {
+        ...CoreMessageFields
+    }
+}`
 const NEW_MESSAGE_SUBSCRIPTION = gql`
 ${CORE_MESSAGE_FIELDS}
 ${CORE_USER_FIELDS}
@@ -51,5 +59,5 @@ query GetMessageThreads($skip:Int, $limit:Int){
     }
 }
 `
-const inboxQueries = { NEW_MESSAGE_SUBSCRIPTION, GET_MESSAGE_THREADS, NEW_MESSAGE_THREAD_SUBSCRIPTION }
+const inboxQueries = { NEW_MESSAGE_SUBSCRIPTION, GET_MESSAGE_THREADS, NEW_MESSAGE_THREAD_SUBSCRIPTION, ON_MESSAGE_UPDATE_SUBSCRIPTION }
 export default inboxQueries;
