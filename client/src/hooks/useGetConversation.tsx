82ccc45d46bd418fa90b91c47294cb0fd388b2e7
amitvolga <amit.shimon@gmail.com>
2022-10-10 19:29:57 +0300

    Fix: move subscription logic inside hook

--- client/src/hooks/useGetConversation.tsx
+++ client/src/hooks/useGetConversation.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client'
+import { useEffect } from 'react';
 import apolloQueries from '../queries';
 
 export function useGetConversation(messageThreadId?: string, ownerId?: string, onCompleted?: (data: any) => void) {
@@ -11,6 +12,17 @@ export function useGetConversation(messageThreadId?: string, ownerId?: string, o
         skip: !messageThreadId && !ownerId,
         onCompleted
     })
+    useEffect(() => {
+        subscribeToMore({
+            document: apolloQueries.inboxQueries.NEW_MESSAGE_SUBSCRIPTION,
+            updateQuery: (prev: any, { subscriptionData }: { subscriptionData: any }) => {
+                const newMessage = subscriptionData?.data?.newMessage?.messages?.[0];
+                if (!newMessage) return prev;
+                const newData = { ...prev, getConversation: { ...prev.getConversation, messages: [newMessage, ...prev.getConversation.messages] } };
+                return newData;
+            }
+        })
+    }, []);
     const fetchMoreMessages = async (skip: number) => {
         const { data: { getConversation } } = await fetchMore({
             variables: {
