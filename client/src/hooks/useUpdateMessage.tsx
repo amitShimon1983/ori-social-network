f1c29862f1f01f467c26a19116f6253ffc255dcf
amitvolga <amit.shimon@gmail.com>
2022-10-17 00:02:04 +0300

    Add: adding reaction id to update

--- client/src/hooks/useUpdateMessage.tsx
+++ client/src/hooks/useUpdateMessage.tsx
@@ -3,11 +3,11 @@ import apolloQueries from '../queries'
 
 export function useUpdateMessage() {
     const [updateMessageMutation, { data, error, loading }] = useMutation(apolloQueries.messagesQueries.UPDATE_MESSAGE)
-    const updateMessage = async (messageId: string, skip: boolean, reactionId?: string) => {
+    const updateMessage = async (messageId: string, skip: boolean, isRead?: boolean, reactionId?: string) => {
         if (!skip) {
             await updateMessageMutation({
                 variables: {
-                    isRead: true,
+                    isRead,
                     reactionId,
                     id: messageId
                 }
