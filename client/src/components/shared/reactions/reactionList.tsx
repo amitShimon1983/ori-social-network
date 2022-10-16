ece9f4ca20b4eac41ff00bf3926113dcb09d4271
amitvolga <amit.shimon@gmail.com>
2022-10-16 18:08:17 +0300

    Fix: adding popover props, item click

--- client/src/components/shared/reactions/reactionList.tsx
+++ client/src/components/shared/reactions/reactionList.tsx
@@ -1,11 +1,14 @@
-import { PopoverOrigin } from "@mui/material";
+import { PopoverOrigin, SxProps, Theme } from "@mui/material";
 import { FunctionComponent, useState } from "react";
-import { BiSmile, Button, Popover } from "..";
-interface Reaction { emoji: string; _id: string }
+import { BiSmile, IconButton, Popover } from "..";
+import { Reaction } from "../../../models";
+import classes from './index.module.css';
 interface ReactionListProps {
     id: string;
     transformOrigin?: PopoverOrigin | undefined;
     anchorOrigin?: PopoverOrigin | undefined;
+    onItemClick: (item: Reaction) => Promise<void> | void;
+    popoverSx?: SxProps<Theme>;
 }
 const reactions: Reaction[] = [
     { emoji: '❤️', _id: '1' },
@@ -16,7 +19,7 @@ const reactions: Reaction[] = [
     { emoji: '👍', _id: '7' },
     { emoji: '👎', _id: '8' }
 ];
-const ReactionList: FunctionComponent<ReactionListProps> = ({ id, transformOrigin, anchorOrigin }) => {
+const ReactionList: FunctionComponent<ReactionListProps> = ({ popoverSx, onItemClick, id, transformOrigin, anchorOrigin }) => {
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -29,11 +32,17 @@ const ReactionList: FunctionComponent<ReactionListProps> = ({ id, transformOrigi
 
     const open = Boolean(anchorEl);
     return (
-        <div>
-            <Button handleClick={handleClick}>
+        <div className={classes.container}>
+            <IconButton className={classes.button} onClick={handleClick}>
                 <BiSmile />
-            </Button>
+            </IconButton>
             <Popover
+                sx={popoverSx ?? {
+                    ".MuiPopover-paper": {
+                        borderRadius: '20px',
+                        padding: '10px'
+                    }
+                }}
                 id={'ReactionList_' + id}
                 open={open}
                 anchorEl={anchorEl}
@@ -48,7 +57,7 @@ const ReactionList: FunctionComponent<ReactionListProps> = ({ id, transformOrigi
                 }}
             >
                 {reactions.map((reaction: Reaction) => {
-                    return <span key={reaction._id}>{reaction.emoji}</span>
+                    return <span onClick={() => onItemClick(reaction)} key={reaction._id}>{reaction.emoji}</span>
                 })}
             </Popover>
         </div>
