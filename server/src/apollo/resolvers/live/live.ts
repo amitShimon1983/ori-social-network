006b4ebc4cd61e1138f7cf8089f437432f093280
amitvolga <amit.shimon@gmail.com>
2022-10-22 17:12:30 +0300

    ADD: adding logic for live calls

--- server/src/apollo/resolvers/live/live.ts
+++ server/src/apollo/resolvers/live/live.ts
@@ -1,19 +1,42 @@
 import { Arg, Ctx, Mutation, Resolver, Root, Subscription } from "type-graphql";
 import { AppContext, UserModel } from "../../../model";
-import { ON_CALL_START } from "../../../utils";
-import { StartCall, StartCallArgs } from "./types";
+import { ON_CALL_ANSWER, ON_CALL_START, ON_ICE_CANDIDATE } from "../../../utils";
+import { CallDetails, IceCandidate, SendIceCandidateArgs, StartCallArgs } from "./types";
 
 
 @Resolver()
 export class LiveResolver {
-    @Subscription(() => StartCall, {
-        topics: [ON_CALL_START],
-        filter: ({ payload, context }) => {
+
+    @Subscription(() => IceCandidate, {
+        topics: [ON_ICE_CANDIDATE],
+        filter: ({ payload, context }: { payload: IceCandidate, context: AppContext }) => {
             const { user } = context;
             return (payload?.addressee === user.email)
         }
     })
-    async onCallStart(@Root() payload: StartCall): Promise<StartCall> {
+    async onIceCandidate(@Root() payload: IceCandidate): Promise<IceCandidate> {
+        return payload;
+    }
+    @Subscription(() => CallDetails, {
+        topics: [ON_CALL_START],
+        filter: ({ payload, context }: { payload: CallDetails, context: AppContext }) => {
+            const { user } = context;
+            return (payload?.addressee === user.email)
+        }
+    })
+    async onCallStart(@Root() payload: CallDetails): Promise<CallDetails> {
+        return payload;
+    }
+
+    @Subscription(() => CallDetails, {
+        topics: [ON_CALL_ANSWER],
+        filter: ({ payload, context }: { payload: CallDetails, context: AppContext }) => {
+            const { user } = context;
+            return (payload?.addressee === user.email)
+        }
+    })
+
+    async onCallAnswer(@Root() payload: CallDetails): Promise<CallDetails> {
         return payload;
     }
 
@@ -23,6 +46,31 @@ export class LiveResolver {
         const userDb = await UserModel.findOne({ email: args.addressee })
         if (userDb) {
             pubSub.publish(ON_CALL_START, {
+                caller: user.email,
+                ...args
+            });
+        }
+        return true;
+    }
+
+    @Mutation(() => Boolean)
+    async answerCall(@Arg('args', () => StartCallArgs) args: StartCallArgs, @Ctx() context: AppContext) {
+        const { user, pubSub } = context;
+        const userDb = await UserModel.findOne({ email: args.addressee })
+        if (userDb) {
+            pubSub.publish(ON_CALL_ANSWER, {
+                caller: user.email,
+                ...args
+            });
+        }
+        return true;
+    }
+    @Mutation(() => Boolean)
+    async sendIceCandidate(@Arg('args', () => SendIceCandidateArgs) args: SendIceCandidateArgs, @Ctx() context: AppContext) {
+        const { user, pubSub } = context;
+        const userDb = await UserModel.findOne({ email: args.addressee })
+        if (userDb) {
+            pubSub.publish(ON_ICE_CANDIDATE, {
                 caller: user.email,
                 ...args
             });
--- server/src/apollo/resolvers/live/types.ts
+++ server/src/apollo/resolvers/live/types.ts
@@ -7,12 +7,35 @@ export class StartCallArgs {
     @Field(() => String)
     sdp: string;
 }
+@InputType()
+export class SendIceCandidateArgs {
+    @Field(() => String)
+    addressee: string;
+    @Field(() => String)
+    icecandidate: string;
+}
 @ObjectType()
-export class StartCall {
+export class IceCandidate {
+    @Field(() => String)
+    addressee: string;
+    @Field(() => String)
+    caller: string;
+    @Field(() => String)
+    icecandidate: string;
+}
+@ObjectType()
+export class CallDetails {
     @Field(() => String)
     addressee: string;
     @Field(() => String)
     sdp: string;
     @Field(() => String)
     caller: string;
+}
+@InputType()
+export class AnswerCallArgs {
+    @Field(() => String)
+    addressee: string;
+    @Field(() => String)
+    sdp: string;
 }
